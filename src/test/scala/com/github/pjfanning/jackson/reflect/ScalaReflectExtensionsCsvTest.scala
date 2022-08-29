06d8371c94a72b0df40bd966eed0544b9f8809fa
PJ Fanning <pjfanning@users.noreply.github.com>
2022-08-29 13:35:14 +0100

    update

--- src/test/scala/com/github/pjfanning/jackson/reflect/ScalaReflectExtensionsCsvTest.scala
+++ src/test/scala/com/github/pjfanning/jackson/reflect/ScalaReflectExtensionsCsvTest.scala
@@ -1,6 +1,7 @@
 package com.github.pjfanning.jackson.reflect
 
-import com.fasterxml.jackson.dataformat.csv.CsvMapper
+import com.fasterxml.jackson.databind.MappingIterator
+import com.fasterxml.jackson.dataformat.csv.{CsvMapper, CsvSchema}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -14,17 +15,25 @@ object CsvMapperScalaReflectExtensions {
 
 class ScalaReflectExtensionsCsvTest extends AnyFlatSpec with Matchers {
   "A CsvMapper with ScalaReflectExtensions mixin" should "deserialize WrappedSeqLong" in {
-    val mapper = newCsvMapperWithScalaReflectExtensions
-    val schema = mapper.schemaFor(mapper.constructType[OptionLong])
+    val csvMapper = newCsvMapperWithScalaReflectExtensions
+    val javaType = csvMapper.constructType[OptionLong]
+    val schema = csvMapper.schemaFor(javaType).withHeader()
     val w1 = OptionLong(Some(100L))
-    val t1 = mapper.writer(schema).writeValueAsString(w1)
-    val v1 = mapper.readValue[OptionLong](t1)
+    val t1 = csvMapper.writer(schema).writeValueAsString(w1)
+    val readerSchema = CsvSchema.builder().setUseHeader(true).setReorderColumns(true).build()
+    val readerIterator: MappingIterator[OptionLong] = csvMapper.readerFor(javaType).`with`(readerSchema).readValues(t1)
+    readerIterator.hasNext shouldEqual true
+    val v1 = readerIterator.next()
     v1 shouldEqual w1
     useOptionLong(v1.valueLong) shouldEqual 200L
+    readerIterator.hasNext shouldEqual false
   }
 
   private def newCsvMapperWithScalaReflectExtensions: CsvMapper with ScalaReflectExtensions = {
-    CsvMapper.builder().addModule(DefaultScalaModule).build() :: CsvMapperScalaReflectExtensions
+    //CsvMapper.builder().addModule(DefaultScalaModule).build() :: CsvMapperScalaReflectExtensions
+    val mapper = new CsvMapper() with ScalaReflectExtensions
+    mapper.registerModule(DefaultScalaModule)
+    mapper
   }
 
   private def useOptionLong(v: Option[Long]): Long = v.map(_ * 2).getOrElse(0L)
